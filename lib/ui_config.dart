02e79b556b30b477c15de73f10ae8fb36033a055
CUMT-Atom <cnatom@foxmail.com>
2022-10-21 23:01:58 +0800

    ✨feature(app):全局UI配置

---
+++ lib/ui_config.dart
@@ -0,0 +1,13 @@
+class UIConfig{
+  static double paddingTB = 16;
+  static double paddingRL = 16;
+  static double marginRL = 16;
+  static double marginTB = 16;
+
+  // 文字大小
+  static double fontSizeMain = 16;
+
+  // 图标大小
+  static double iconSizeMain = 16;
+
+}
