19be25fc0c4de8cce9154a68990a42155af6e900
CUMT-Atom <cnatom@foxmail.com>
2022-10-21 23:02:40 +0800

    ✨feature(main):健全路由

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/course1_ui.dart';
+import 'package:flutter_learning/ui_config.dart';
 
 main() => runApp(const MyApp());
 
@@ -7,9 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false, // 隐藏debug横幅
-      home: HomePage(),
+      routes: {
+        Course1UI.id:(context)=>const Course1UI()
+      },
+      home: const HomePage(),
     );
   }
 }
@@ -17,13 +22,39 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  final EdgeInsetsGeometry padding = const EdgeInsets.symmetric(horizontal: 16,vertical: 16);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter培训项目记录"),
       ),
+      body: ListView(
+        children: [
+          _buildRowButton(context,Course1UI.id,Course1UI.title)
+        ],
+      ),
     );
   }
+
+  InkWell _buildRowButton(BuildContext context,String id,String title) {
+    return InkWell(
+      onTap: ()=>Navigator.pushNamed(context, id),
+          child: Padding(
+            padding: padding,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title,style: TextStyle(fontSize: UIConfig.fontSizeMain),),
+                Icon(Icons.chevron_right,size: UIConfig.iconSizeMain,)
+              ],
+            ),
+          ),
+        );
+  }
+
+
 }
 
